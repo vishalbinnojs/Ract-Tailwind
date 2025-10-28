1e2b3f21926a702f8d080ca22e807f15bd4701c8
Vishal Bhardwaj <vishalbinnojs@gmail.com>
2025-10-28 12:36:24 +0530

    result ui updated

--- Quiz_app/src/components/Result.jsx
+++ Quiz_app/src/components/Result.jsx
@@ -4,7 +4,8 @@ const Result = ({children}) => {
     
   return (
     <>
-    <h1 className="text-center text-3xl font-bold">Your Score is: {children}</h1>
+    <h1 className="text-center text-3xl font-extralight leading-10 ">🎉Congratulations🎊 <br />
+      Your Score is: {children}</h1>
     </>
   )
 };
