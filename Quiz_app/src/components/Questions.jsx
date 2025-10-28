3b8b59d2fcf0f155a84d3f1cb84dd8213ebdb545
Vishal Bhardwaj <vishalbinnojs@gmail.com>
2025-10-28 12:07:33 +0530

    ui updated

--- Quiz_app/src/components/Questions.jsx
+++ Quiz_app/src/components/Questions.jsx
@@ -9,41 +9,34 @@ import { IoCheckmarkDoneCircle } from "react-icons/io5";
 const Questions = ({ getScore, setShowResult }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [score, setScore] = useState(0);
-  console.log("score", score);
-  const [selectedAnswers, setSelectedAnswers] = useState({});  
-  const [currentAnswer,setCurrentAnswer] = useState(null);
-  console.log("selectedAnswers", selectedAnswers)
-console.log("currentAnswer",currentAnswer);
+  // console.log("score", score);
+  const [selectedAnswers, setSelectedAnswers] = useState({});
+  const [currentAnswer, setCurrentAnswer] = useState(null);
+  // console.log("selectedAnswers", selectedAnswers)
+  // console.log("currentAnswer",currentAnswer);
 
-const [submitButton, setSubmitButton] = useState(false);
-
+  const [submitButton, setSubmitButton] = useState(false);
 
   const handleOptionClick = (option) => {
     const correctAnswer = questions[currentIndex].answer;
 
-      setCurrentAnswer(option);
+    setCurrentAnswer(option);
 
-    
-  setSelectedAnswers((prev) => {
-    const alreadyAnswered = prev[currentIndex];
-    const isPreviouslyCorrect = alreadyAnswered === correctAnswer;
-    const isNowCorrect = option === correctAnswer;
+    setSelectedAnswers((selectedAnswer) => {
+      const alreadyAnswered = selectedAnswer[currentIndex];
+      const isPreviouslyCorrect = alreadyAnswered === correctAnswer;
+      const isNowCorrect = option === correctAnswer;
 
-    // Score logic based on change
-    if (!isPreviouslyCorrect && isNowCorrect) {
-      setScore(prevScore => prevScore + 1);
-    } else if (isPreviouslyCorrect && !isNowCorrect) {
-      setScore(prevScore => prevScore - 1);
-    }
+      // Score logic based on change
+      if (!isPreviouslyCorrect && isNowCorrect) {
+        setScore((prevScore) => prevScore + 1);
+      } else if (isPreviouslyCorrect && !isNowCorrect) {
+        setScore((prevScore) => prevScore - 1);
+      }
 
-    return { ...prev, [currentIndex]: option };
-  });
-        
-};
-        
-    
-
-
+      return { ...selectedAnswer, [currentIndex]: option };
+    });
+  };
 
   const prevQuestion = () => {
     if (currentIndex === 0) {
@@ -51,7 +44,7 @@ const [submitButton, setSubmitButton] = useState(false);
     }
     setCurrentIndex((prevIndex) => prevIndex - 1);
     setSubmitButton(false);
-    setCurrentAnswer(null)
+    setCurrentAnswer(null);
   };
   const nextQuestion = () => {
     if (currentIndex === questions.length - 1) {
@@ -59,25 +52,33 @@ const [submitButton, setSubmitButton] = useState(false);
       return;
     }
     setCurrentIndex((prevIndex) => prevIndex + 1);
-      setCurrentAnswer(null)
+    setCurrentAnswer(null);
   };
 
   return (
     <>
-    <span className=' inline-block mb-2'>Questions : {currentIndex + 1}/{questions.length}</span>
-      <h2 className="text-2xl bg-green-900 p-2 rounded-md">
+      <span className=" inline-block mb-2">
+        Questions : {currentIndex + 1}/{questions.length}
+      </span>
+      <h2 className="text-2xl bg-gray-900 border-2 p-2 rounded-md">
         {questions[currentIndex].question}
       </h2>
       <div className="flex flex-col mt-4">
         {questions[currentIndex].options.map((option, idx) => (
           <button
             key={idx}
-            onClick={() => handleOptionClick(option,idx)}
+            onClick={() => handleOptionClick(option, idx)}
             className={`bg-[#1E293B] m-1 p-3 rounded-md hover:bg-gray-900 hover:outline-1 cursor-pointer flex justify-between items-center`}
-          ><span>{idx+1}</span>
+          >
+            <span>{idx + 1}</span>
             <span>{option}</span>
             <span className=" bg-green-600 rounded-full ">
-              <IoCheckmarkDoneCircle size={20} className={` ${currentAnswer === option ? "block ": "hidden "} `}/>
+              <IoCheckmarkDoneCircle
+                size={20}
+                className={` ${
+                  currentAnswer === option ? "block " : "hidden "
+                } `}
+              />
             </span>
           </button>
         ))}
