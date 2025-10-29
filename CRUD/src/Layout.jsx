0932ea553c2d4b64f1fd9db4f2916eb2e295032e
Vishal Bhardwaj <vishalbinnojs@gmail.com>
2025-10-29 10:52:50 +0530

    layout update

--- CRUD/src/Layout.jsx
+++ CRUD/src/Layout.jsx
@@ -2,7 +2,7 @@ import { Outlet, Link } from "react-router-dom";
 
 function Layout() {
   return (
-    <div>
+    <>
       <nav className="p-4 bg-gray-700 flex gap-4 text-white">
         <Link to="/">Home</Link>
         <Link to="/crud1">CRUD1</Link>
@@ -13,7 +13,7 @@ function Layout() {
         {/* Child route will be rendered here */}
         <Outlet />
       </main>
-    </div>
+    </>
   );
 }
 
