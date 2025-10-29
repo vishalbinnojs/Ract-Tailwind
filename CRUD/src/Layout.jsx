e2d403853e03a7267294d87cd0113508e9a8afe3
Vishal Bhardwaj <vishalbinnojs@gmail.com>
2025-10-29 10:53:51 +0530

    Layout update

--- CRUD/src/Layout.jsx
+++ CRUD/src/Layout.jsx
@@ -3,11 +3,13 @@ import { Outlet, Link } from "react-router-dom";
 function Layout() {
   return (
     <>
-      <nav className="p-4 bg-gray-700 flex gap-4 text-white">
+    <header>
+        <nav className="p-4 bg-gray-700 flex gap-4 text-white">
         <Link to="/">Home</Link>
         <Link to="/crud1">CRUD1</Link>
         <Link to="/crud2">CRUD2</Link>
       </nav>
+    </header>
 
       <main className="">
         {/* Child route will be rendered here */}
